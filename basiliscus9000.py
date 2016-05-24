144c0cd134d50a1f8087055d973912ff5942af62
annakertesz <kerteszannanak@gmail.com>
2016-05-24 13:28:02 +0200

    basic game with pep8

--- basiliscus9000.py
+++ basiliscus9000.py
@@ -1,4 +1,7 @@
-import time, harrypotter, hpintro, curses
+import time
+import harrypotter
+import hpintro
+import curses
 from curses import wrapper
 curses.curs_set(0)
 screen = curses.initscr()
@@ -6,14 +9,14 @@ dims = screen.getmaxyx()
 bye = "Thanks for helping Mardekar to clean Hogwarts"
 mb = 0
 pb = 0
-quit=0
+quit = 0
 life = 3
-snake = [[10,10], [10,11], [10,12]]
+snake = [[10, 10], [10, 11], [10, 12]]
 c = 100
 hpintro.intro()
 while harrypotter.canexit():
     harrypotter.move()
 screen.clear()
-screen.addstr(5, int((dims[1])/2-len(bye)/2), str(bye))
+screen.addstr(5, int((dims[1]) / 2 - len(bye) / 2), str(bye))
 screen.refresh()
 time.sleep(4)
